0748e1b7c51804d1c1ad25b579cd6e7261cf15b4
Vadim <shonistro@gmail.com>
2024-08-06 02:45:07 +0300

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./layout/header/header.component";
+import {SidebarComponent} from "./layout/sidebar/sidebar.component";
+import {SidebarService} from "./layout/sidebar/sidebar.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
+  providers: [SidebarService],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
